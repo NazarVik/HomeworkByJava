35ed89538fc5f08a39c22814a838f008d1d4055a
NazarVik <Nazarov_dev@icloud.com>
2023-03-12 21:31:34 +0300

    ready homework seminar 3

---
+++ src/Seminar3/Task2.java
@@ -0,0 +1,10 @@
+package Seminar3;
+
+
+public class Task2 {
+    //    Пусть дан произвольный список целых чисел, удалить из него чётные числа
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ src/Seminar3/Task3.java
@@ -0,0 +1,8 @@
+package Seminar3;
+
+public class Task3 {
+//    Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+    public static void main(String[] args) {
+
+    }
+}
