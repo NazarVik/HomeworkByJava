c280ab8fd03fd83c7a1223d0cd0a9f74c89db384
NazarVik <Nazarov_dev@icloud.com>
2023-03-22 22:09:07 +0300

    ready homework seminar 4

--- src/Seminar6/Task1.java
+++ src/Seminar6/Task1.java
@@ -6,98 +6,95 @@ public class Task1 {
     public static void main(String[] args) {
         List<Notebook> list = new ArrayList<>();
 
-        Notebook note1 = new Notebook("Lenova", 17.0, "i5", "Windows", 8, 128);
-        Notebook note2 = new Notebook("Asus", 15.6, "i7", "Windows", 16, 128);
-        Notebook note3 = new Notebook("HP", 15.6, "i3", "Linux", 8, 128);
-        Notebook note4 = new Notebook("Macbook", 13.3, "i5", "IOS", 8, 254);
-        Notebook note5 = new Notebook("Macbook", 13.3, "i3", "IOS", 4, 128);
-        Notebook note6 = new Notebook("Acer", 15.6, "i3", "Windows", 4, 128);
+        Notebook note1 = new Notebook("lenova", 17.0, "i5", "windows", 8, 128);
+        Notebook note2 = new Notebook("asus", 15.6, "i7", "windows", 16, 128);
+        Notebook note3 = new Notebook("hp", 15.6, "i3", "linux", 8, 128);
+        Notebook note4 = new Notebook("macbook", 13.3, "i5", "ios", 8, 254);
+        Notebook note5 = new Notebook("macbook", 13.3, "i3", "ios", 4, 128);
+        Notebook note6 = new Notebook("macbook", 15.6, "i5", "ios", 8, 128);
+        Notebook note7 = new Notebook("acer", 15.6, "i3", "windows", 4, 128);
 
-        Collections.addAll(list, note1, note2, note3, note4, note5, note6);
+        Collections.addAll(list, note1, note2, note3, note4, note5, note6, note7);
 
-        Map<Integer, String > mapChoice = new HashMap<>();
-//        mapChoice.put(1, "model");
-//        mapChoice.put(2, "size");
-//        mapChoice.put(3, "cpu");
-//        mapChoice.put(4, "os");
-//        mapChoice.put(5, "ram");
-//        mapChoice.put(6, "ssd");
-
-        System.out.print("введите критерий сортировки: \n1 - модель \n" +
+        System.out.print("\n1 - модель\n" +
                 "2 - диогональ экрана\n" +
                 "3 - процессор\n" +
                 "4 - операционная система\n" +
                 "5 - оперативная память\n" +
                 "6 - объем накопителя\n" +
-                "--> ");
+                "введите критерий сортировки через пробел: ");
 
         Scanner in = new Scanner(System.in);
-        int choice = in.nextInt();
+        String[] choice = in.nextLine().split(" ");
 
-        switch(choice) {
-            case 1: {
-                System.out.print("введите модель (Acer, Lenova, HP, Asus, Macbook, MacbooK): ");
-                modelSort(list, in.next());
-                break;
-            }
-            case 2: {
-                System.out.print("введите размер диогонали (13.3, 15.6, 17.0): ");
-                sizeSort();
-                break;
-            }
-            case 3: {
-                System.out.print("введите модель процессора (i3, i5, i7): ");
-                cpuSort();
-                break;
-            }
-            case 4: {
-                System.out.println();
-                osSort();
-                break;
-            }
-            case 5: {
-                ramSort();
-                break;
-            }
-            case 6: {
-                ssdSort();
-                break;
-            }
-
-            default:
-                System.out.println("fail");
-        }
+        for (String str : choice) {
 
 
+            switch (str) {
+                case "1": {
+                    System.out.print("введите модель (acer, lenova, hp, asus, macbook): ");
+                    modelSort(list, in.next());
+                    break;
+                }
+                case "2": {
+                    System.out.print("введите размер диогонали (13.3, 15.6, 17.0): ");
+//                    sizeSort(copyList, in.next().toLowerCase());
+                    break;
+                }
+                case "3": {
+                    System.out.print("введите модель процессора (i3, i5, i7): ");
+//                    cpuSort(copyList, in.next().toLowerCase());
+                    break;
+                }
+                case "4": {
+                    System.out.println("введите операционную систему (Linux, : ");
+//                    osSort(copyList, in.next().toLowerCase());
+                    break;
+                }
+                case "5": {
+                    System.out.print("введите объем ОЗУ: ");
+                    ramSort(list, in.next());
+                    break;
+                }
+                case "6": {
+                    System.out.println();
+//                    ssdSort(copyList, in.next().toLowerCase());
+                    break;
+                }
 
-    }
-
-    private static void modelSort(List<Notebook> list, String str){
-
-        List<Notebook> resultList = new ArrayList<>();
-
-        for (Notebook item : list) {
-            if (str.equals(item.getModel())) {
-                resultList.add(item);
+                default:
+                    System.out.println("fail");
+                    break;
             }
         }
-        System.out.println(resultList);
+        System.out.println(list);
+
 
     }
 
-    private static void sizeSort(){
+    private static void modelSort(List<Notebook> list, String str) {
+
+        list.removeIf(item -> !str.equals(item.getModel()));
+    }
+
+    private static void sizeSort() {
 
     }
-    private static void cpuSort(){
+
+    private static void cpuSort() {
 
     }
-    private static void osSort(){
+
+    private static void osSort() {
 
     }
-    private static void ramSort(){
 
+    private static void ramSort(List<Notebook> list, String str) {
+
+        list.removeIf(item -> !(item.getRam() == Integer.parseInt(str)));
     }
-    private static void ssdSort(){
+
+    private static void ssdSort() {
 
     }
 }
